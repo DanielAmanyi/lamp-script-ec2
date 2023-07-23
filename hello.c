e5f0253bdb7c21bf7ef31b1053146d0b2c5573c5
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-23 19:55:40 +0000

    Sun, Jul 23, 2023, 8:55 PM +01:00

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@
 int main(void)
 
 {
-    string input_name = get_string ("what is your Last name?  ");
-    printf ("Hello, and welcome %s\n", input_name);
+    string input_last_name = get_string ("what is your Last name?  ");
+    string input_first_name = get_string ("what is your First name?  ");
+    printf ("Hello, and welcome %s %s\n", input_last_name, input_first_name);
 
 }
