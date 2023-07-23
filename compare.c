7315890aacb59ba8a967490272bd6885c2428063
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-23 21:38:55 +0000

    Sun, Jul 23, 2023, 10:38 PM +01:00

---
+++ compare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+int x = get_int ("input first value ");
+int y = get_int ("input second value ");
+if (x<y)
+{
+printf ( "x is less than y . ");
+}
+
+}
