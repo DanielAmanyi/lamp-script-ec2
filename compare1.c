79e6eec7508f2bfc02003f3926255039a8b1de93
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-24 01:36:50 +0000

    Mon, Jul 24, 2023, 2:36 AM +01:00

--- compare1.c
+++ compare1.c
@@ -2,21 +2,19 @@
 #include<cs50.h>
 
 int main(void)
-
 {
-int first_number = get_int ("insert your first number   ");
-int second_number = get_int ("insert your second number   ");
+int first_number = get_int ("Please enter your first number \n");
+int second_number = get_int ("Please enter your second number \n");
 
-if (first_number < second_number)
+if (first_number<second_number)
 {
-    printf ( "Second Number is greater than Firstnumber \n" );
+printf("Second Number is Larger \n");
 }
-if (first_number > second_number)
+if (first_number>second_number)
 {
-    printf ( "First Number is Greter than Second Number \n" );
+printf ("First number is Larger \n");
 }
 else
-{
-        printf ( "The two numbers are Exact and same \n" );
+{printf("both numbers are equal \n");
 }
 }
