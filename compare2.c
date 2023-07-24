0d44544e814b1e8e5107d32d2170ca3ad8ce0be2
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-24 00:39:34 +0000

    Mon, Jul 24, 2023, 1:39 AM +01:00

---
+++ compare2.c
@@ -0,0 +1,20 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main (void)
+{
+    int x = get_int ("insert first number  \n");
+    int y = get_int ("insert second number  \n");
+}
+if (y<x)
+{
+    print (" x is greater than y \n");
+}
+if y>x
+{
+    print ("y is greater than x \n");
+}
+else
+{
+    print ("y is eaqual to x  \n");
+}
