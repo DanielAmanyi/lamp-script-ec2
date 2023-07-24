8daefd7b9363cc6920cf9713e154b7c1a402bdd9
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-24 01:47:01 +0000

    Mon, Jul 24, 2023, 2:47 AM +01:00

--- compare1.c
+++ compare1.c
@@ -7,11 +7,11 @@ int First_number = get_int ("Please enter your first number \n");
 int second_number = get_int ("Please enter your second number \n");
 
 if (First_number < second_number)
- {
+{
 printf("Second Number is Larger \n");
 }
 if (First_number > second_number)
- {
+{
 printf ("First number is Larger \n");
 }
 else
