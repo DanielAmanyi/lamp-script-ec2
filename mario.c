9c19ef74b448bc0a14d6b22d6f4c3a1e772a6986
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-07-25 16:36:45 +0000

    Tue, Jul 25, 2023, 5:36 PM +01:00

--- mario.c
+++ mario.c
@@ -5,10 +5,10 @@ int main (void)
 {
     for (int b = 0; b <5; b++)
     {
-        printf (" # \n");\
-        }
         for (int a = 0; a <5; a++)
-    {
+        {
+            printf ("#");\
+        }
     printf (" \n");
     }
 }
