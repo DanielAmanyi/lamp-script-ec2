d11fb8384aa7c4cb16a5ba9ee74d1f5936056c07
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-04 17:10:30 +0000

    Fri, Aug 4, 2023, 6:10 PM +01:00

--- population/population.c
+++ population/population.c
@@ -1,6 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void flush_input_buffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 int main(void)
 {
     // TODO: Prompt for start size
@@ -8,23 +14,22 @@ int main(void)
 
     do
     {
-        start_size = get_int (" Enter Starting Population \n");
+        start_size = get_int("Enter Starting Population: ");
+        flush_input_buffer(); // Flush the input buffer
     }
     while (start_size < 9);
 
-
-
     // TODO: Prompt for end size
-     int end_size;
+    int end_size;
 
     do
     {
-        end_size = get_int (" Enter ending Population \n")
+        end_size = get_int("Enter ending Population: ");
+        flush_input_buffer(); // Flush the input buffer
     }
     while (end_size < 9);
 
-    return 0;
-    // TODO: Calculate number of years until we reach threshold
+    // Rest of your code...
 
-    // TODO: Print number of years
+    return 0;
 }
