7cd72fe69643b3dd8afeba4c64976569c149c358
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-04 17:17:09 +0000

    Fri, Aug 4, 2023, 6:17 PM +01:00

--- population/population.c
+++ population/population.c
@@ -6,20 +6,20 @@ int main(void)
     // TODO: Prompt for start size
     int start_size;
 
-    do
+     while (start_size < 9);
     {
         start_size = get_int (" Enter Starting Population \n");
     }
-    while (start_size < 9);
+
 
     // TODO: Prompt for end size
      int end_size;
 
-    do
+   
     {
         end_size = get_int (" Enter ending Population \n")
     }
-    while (end_size < 9);
+
 
     return 0;
     // TODO: Calculate number of years until we reach threshold
