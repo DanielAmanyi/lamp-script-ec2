9677ed62a068f244dc0ed65cb9a2b5ecea80b65c
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-05 18:44:37 +0000

    Sat, Aug 5, 2023, 7:44 PM +01:00

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+
+{
+void sum();
+}
+{
+    int a = get_int("Enter First number \n");
+    int b = get_int ("enter Second Number \n");
+    int sum = (a+b);
+
+    {
+        printf("The Sum is: %d \n", sum);
+    }
+}
