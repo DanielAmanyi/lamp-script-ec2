fae9be705f265accd4d7018fe5414f746d8de1d0
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-06 15:15:04 +0000

    Sun, Aug 6, 2023, 4:15 PM +01:00

--- debug/debug.c
+++ debug/debug.c
@@ -6,9 +6,12 @@
 int main(void)
 {
     // Ask for your name and where live
-    char name = get_string("What is your name? ");
-    char location = get_string("Where do you live? ");
+    string name = get_string("What is your name? ");
+
+    string location = get_string("Where do you live? ");
 
     // Say hello
-    printf("Hello, %i, from %i!", name, location);
+    {
+        printf("Hello, %i, from %i!", name, location);
+    }
 }
