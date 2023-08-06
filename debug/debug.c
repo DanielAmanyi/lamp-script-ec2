56a765029ffaf5bb3c46872c61f7d86323293d6f
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-06 15:30:11 +0000

    Sun, Aug 6, 2023, 4:30 PM +01:00

--- debug/debug.c
+++ debug/debug.c
@@ -1,8 +1,7 @@
 // Become familiar wih C syntax
 // Learn to debug buggy code
 #include <cs50.h>
-#include<stdio.h>
-int main(void)
+#include<stdio.h>int main(void)
 {
     // Ask for your name and where live
     string name = get_string("What is your name? ");
