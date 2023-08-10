8e5a8f9b2ac0d499cca011ac0636d663f96cacec
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2023-08-10 21:07:40 +0000

    Thu, Aug 10, 2023, 10:07 PM +01:00

--- Practice/hello.c
+++ Practice/hello.c
@@ -5,9 +5,10 @@
 
 int main(void)
 {
-    for (int i = 0; i < 4; i++)
+    int n = get_int ("How many array grid do you want to see? \n");
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("#");
         }
