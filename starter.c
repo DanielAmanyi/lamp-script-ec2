069bbd15442d08ef2c19e13877089f0e2e1c93e5
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-02-24 07:37:53 +0000

    Sat, Feb 24, 2024, 8:37 AM +01:00

--- starter.c
+++ starter.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include<cs50.h>
+#include<stdio.h>
 
-int main(void)
+int main(void);
 
-{
-    printf("Hello Super \n") ;
-}
+string firstname = get_string("what's your first name? \n");
+string lastname = get_string("your Surname?  \n");
+printf("Hello %s, %s" firstname, lastname);
