5dd9a202835571013c5c7cca9e697bc36c334496
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-02-24 07:42:24 +0000

    Sat, Feb 24, 2024, 8:42 AM +01:00

--- starter.c
+++ starter.c
@@ -7,5 +7,5 @@ int main(void)
 {
 string firstname = get_string("what's your first name? \n");
 string lastname = get_string("your Surname?  \n");
-printf("Hello %s, %s" firstname, lastname);
+printf("Hello, %s, %s", firstname, lastname);
 }
