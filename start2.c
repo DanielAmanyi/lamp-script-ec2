32663ecf073b7da5e4838e8a0b157a7a7e104502
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-10 18:59:46 +0000

    Sun, Mar 10, 2024, 7:59 PM +01:00

--- start2.c
+++ start2.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int main(void);
+int main(void)
 {
  int a = get_int("Enter your grid size \n");
 
 for (int i = 0; 1<a; i++)
 {
-printf("#")
+printf("#");
 }
 }
