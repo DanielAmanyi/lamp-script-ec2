b9dcdad8d37d6c43fa728d58bb51a30e12f86e4a
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-13 15:34:50 +0000

    Wed, Mar 13, 2024, 4:34 PM +01:00

--- Population/population.c
+++ Population/population.c
@@ -9,15 +9,16 @@ int start = get_int("Enter Start Size \n");
 
     // TODO: Prompt for end size
 
-int end = get_int("Stop Size \n");
+// int end = get_int("Stop Size \n");
 
     // TODO: Calculate number of years until we reach threshold
     double start_years, end_years;
     int Total_lamas;
 
     start_years = start/3;
-    end_years = end/4;
+    end_years = start/4;
     Total_lamas = end_years - start_years;
+    start_lamas+
 
     // TODO: Print number of years
     printf(" Lamas left would be %lf \n", Total_lamas);
