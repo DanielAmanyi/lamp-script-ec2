a88975d4d14f32b5e9794bbbca31eef584fde443
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-13 15:14:48 +0000

    Wed, Mar 13, 2024, 4:14 PM +01:00

--- Population/population.c
+++ Population/population.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int main(void) {
-get_int = ("enter the number of lamas)
+int main(void) {  // TODO: Prompt for start size
+get_int = ("Enter Start Size \n")
+    // TODO: Prompt for end size
+    get_int = ("Stop Size\n")
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+
 
 
 
