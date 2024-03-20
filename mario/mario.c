0d166441090312dcbc9ff1bf116b0b04dd3edd28
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-20 23:01:54 +0000

    Thu, Mar 21, 2024, 12:01 AM +01:00

---
+++ mario/mario.c
@@ -0,0 +1,32 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void){
+int height;
+
+do{
+height = get_int("Enter Pyramid Height  \n");
+
+
+
+
+}
+while (height <1 || >8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
