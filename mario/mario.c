03e09e4d62009114bb1f046f757c32132f33d3d1
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-21 23:32:31 +0000

    Fri, Mar 22, 2024, 12:32 AM +01:00

--- mario/mario.c
+++ mario/mario.c
@@ -1,36 +1,35 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
-// Declare variables
-int main(void){
-int height;
-int count = 0;
-
-
-// Prompt user for Height of Pyramid with a limit of 5 tries to prevent session overstay
-do{
-    if (count == 4){
-     printf("You have one more try \n");
-    }
-height = get_int("Enter Pyramid Height  \n");
-
-
-count++;
-}
-while ((height <1 || height >8) && count <5);
-
-
-int i;
-for (i = 0; i <height; i++)
+int main(void)
 {
-printf("\n");
+    int height;
+    int count = 0;
 
-int j;
-    for (j=0; j<height; j++)
-printf("x");
-}
-printf(" \n");
+    // Prompt user for Height of Pyramid with a limit of 5 tries to prevent session overstay
+    do
+    {
+        if (count == 4)
+        {
+            printf("You have one more try \n");
+        }
+        height = get_int("Enter Pyramid Height  \n");
+        count++;
+    } while ((height < 1 || height > 8) && count < 5);
 
+    // Print the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("x");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
