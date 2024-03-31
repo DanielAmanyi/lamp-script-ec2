00a2d265467d818284cb8cc82761f5ba7c2fb736
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-31 16:26:32 +0000

    Sun, Mar 31, 2024, 5:26 PM +01:00

--- Array/array.c
+++ Array/array.c
@@ -6,14 +6,13 @@ int main (void)
 {
 // int scores = get_int("enter score \n");
 
-
+int scores[3];
 // scores[0] = get_int("Score1: \n");
 // scores[1] = get_int("Score2: \n");
 // scores[2] = get_int("Score3: \n");
 int n=3;
 for (int i=0; i<n; i++)
 {
-    int scores[3];
    scores = get_int("enter score \n");
 }
 
