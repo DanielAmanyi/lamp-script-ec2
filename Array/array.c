9b3ec10eade61784cadc5b84b5e367ae16dcb602
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-31 16:23:30 +0000

    Sun, Mar 31, 2024, 5:23 PM +01:00

--- Array/array.c
+++ Array/array.c
@@ -4,20 +4,20 @@
 
 int main (void)
 {
-int n;
 // int scores = get_int("enter score \n");
 
 int scores[3];
 // scores[0] = get_int("Score1: \n");
 // scores[1] = get_int("Score2: \n");
 // scores[2] = get_int("Score3: \n");
+int n=3;
 for (int i=0; i<n; i++)
 {
-    scores[n] = get_int("enter score \n");
+   scores[n] = get_int("enter score \n");
 }
 
 {
-    printf("Average Equals:  %d \n", (scores[1] + scores[0] + scores[2] )/3);
+    printf("Average Equals:  %d \n", (scores[1] + scores[0] + scores[2] )/n);
 }
 
 
