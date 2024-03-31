f4029336357482d1ab86851c04d5bd1aa0e937fb
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-03-31 14:55:41 +0000

    Sun, Mar 31, 2024, 3:55 PM +01:00

---
+++ Array/array.c
@@ -0,0 +1,23 @@
+#include<cs50.h>
+#include<stdio.h>
+
+
+int main (void)
+{
+
+int scores = get_int("enter score \n");
+
+scores[3];
+scores[0] = 77
+scores[1] =78
+scores[2] =79
+
+
+{
+    printf("%f \n", scores);
+}
+
+
+
+    return 0;
+}
