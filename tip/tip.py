0c2397f5ca0d5ac1f7200e6a4daa92bfe95773ad
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-05 15:58:54 +0000

    Mon, Aug 5, 2024, 4:58 PM +01:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,18 +1,18 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
+    tip = float(dollars) * float(percent)
     print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    # Remove the dollar sign and convert the string to a float
-    return float(d.replace('$', ''))
+    # TODO
+    return float((d).replace('$', ''))
 
 
 def percent_to_float(p):
-    # Remove the percent sign, convert the string to a float, and divide by 100
-    return float(p.replace('%', '')) / 100
+    # TODO
+    return float((p).replace('%', ''))/100
 
 
 main()
