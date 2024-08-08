f2dec9ca791fc5008d1c7646b703d82713f9e63d
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-08 02:03:08 +0000

    Thu, Aug 8, 2024, 3:03 AM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -14,9 +14,31 @@ comprehension = ''' “All right,” said the computer, and settled into silence
 — The Hitchhiker’s Guide to the Galaxy, Douglas Adams
 '''
 
-usr = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
-wrong = "No"
-if usr in ["42", "forty two", "forty-two"]:
-    print("yes")
-else:
-    print(wrong)
+
+
+comprehension = ''' “All right,” said the computer, and settled into silence again. The two men fidgeted. The tension was unbearable.
+“You’re really not going to like it,” observed Deep Thought.
+“Tell us!”
+“All right,” said Deep Thought. “The Answer to the Great Question…”
+“Yes…!”
+“Of Life, the Universe and Everything…” said Deep Thought.
+“Yes…!”
+“Is…” said Deep Thought, and paused.
+“Yes…!”
+“Is…”
+“Yes…!!!…?”
+“Forty-two,” said Deep Thought, with infinite majesty and calm.”
+
+— The Hitchhiker’s Guide to the Galaxy, Douglas Adams
+'''
+
+# ask user a question using an input function
+question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? " ).lower()
+#note that all user input is a string until otherwise converted
+# compare input with system option; "42" , "forty-two", "forty two"
+options = ["42" , "forty-two", "forty two"]
+# logic to compare input with system option
+if question in options:
+    print ("Yes")
+else: print ("No")
+# output function to output result
