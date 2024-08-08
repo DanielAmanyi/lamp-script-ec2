22733212fd41edd6596c058022c10930e58397f3
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-08 02:07:13 +0000

    Thu, Aug 8, 2024, 3:07 AM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -11,7 +11,8 @@ comprehension = ''' “All right,” said the computer, and settled into silence
 “Yes…!”
 “Is…”
 “Yes…!!!…?”
-“Forty-two,” said Deep Thought, with infinite majesty and calm.”
+“Forty-two,” said Deep Thought, win; "42" , "forty-two", "forty two"
+options = ["42" , "forty-two", "fth infinite majesty and calm.”
 
 — The Hitchhiker’s Guide to the Galaxy, Douglas Adams
 '''
@@ -19,8 +20,7 @@ comprehension = ''' “All right,” said the computer, and settled into silence
 # ask user a question using an input function
 question = input("What is the Answer to the Great Question of Life, the Universe, and Everything? " ).lower().strip()
 #note that all user input is a string until otherwise converted
-# compare input with system option; "42" , "forty-two", "forty two"
-options = ["42" , "forty-two", "forty two"]
+# compare input with system optioorty two"]
 # logic to compare input with system option
 if question in options:
     print ("Yes")
