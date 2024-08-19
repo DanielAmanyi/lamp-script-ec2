3c278bc7b2240298e7e485679245c0993d4d2f7e
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-19 16:14:21 +0000

    Mon, Aug 19, 2024, 5:14 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,10 +5,9 @@ acceptable = [25,10,5]
 while z <= 50:
     if z <= 50:
         y = int(input("Insert Coin "))
-        z=y+z
-        print("Amount Due:", x - z)
-    if y in acceptable:
-        continue
-    if x <= z:
-        print("Change Owed:", z -x)
-        break
+        if y in acceptable:
+            z=y+z
+            print("Amount Due:", x - z)
+        if x <= z:
+            print("Change Owed:", z -x)
+            break
