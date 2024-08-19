1e4748a5b6b6a63d88cd1e3a1ea7a59b617d1a7a
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-19 22:51:24 +0000

    Mon, Aug 19, 2024, 11:51 PM +01:00

---
+++ twttr/twttr.py
@@ -0,0 +1,16 @@
+# Vowels Library
+vowels = ["a", "e", "i", "o", "u"]
+
+# Take in user input
+tweet = input("Enter your tweet: ")
+
+# Convert tweet to a list
+tweet = list(tweet)
+
+# Loop through each character in the tweet
+for i in tweet[:]:
+    if i in vowels:
+        tweet.remove(i)  # Remove the vowel
+
+# Convert the list back into a string and print the result
+print("Your tweet without vowels:", ''.join(tweet))
