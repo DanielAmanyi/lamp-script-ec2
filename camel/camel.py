b49014dece9a5de96241c05d539c58e096df847a
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-19 12:17:44 +0000

    Mon, Aug 19, 2024, 1:17 PM +01:00

---
+++ camel/camel.py
@@ -0,0 +1,13 @@
+
+
+def insert_(i):
+    i = ("_"+i).lower()
+    return i
+
+
+camel_case = input("Enter Name ")
+for i in camel_case:
+    if i.isupper():
+        i = insert_(i)
+    print(i,end="")
+print()
