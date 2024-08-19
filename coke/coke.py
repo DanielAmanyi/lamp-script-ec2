a6f2339638a4f2b4191f044867fbcaa34e201079
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-19 17:38:32 +0000

    Mon, Aug 19, 2024, 6:38 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,13 +1,26 @@
-x = 50
-y = 0
-z = 0
-acceptable = [25,10,5]
-while z <= 50:
-    if z <= 50:
-        y = int(input("Insert Coin "))
-        if y in acceptable:
-            z=y+z
-            print("Amount Due:", x - z)
-        if x <= z:
-            print("Change Owed:", z -x)
-            break
+def update_coins(coins):
+    total_coins2 = 0
+    total_coins2=coins+total_coins2
+    return total_coins2
+
+def main():
+    # defaults
+    coke_price = 50
+    coins = 0
+    total_coins = 0
+    acceptable_coins = [25,10,5]
+
+    # take in user input,
+    while total_coins < 50:
+        coins = int(input("Insert Coin "))
+
+        # user input control
+        if coins in acceptable_coins:
+            total_coins = update_coins(coins)+total_coins
+        print("Amount Due:", coke_price - total_coins)
+
+        if total_coins >= coke_price:
+                print("Change Owed:", total_coins -coke_price)
+
+main()
+
