8dc8223d5e55021d28f42f901af11693970f10e7
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-08-23 00:51:50 +0000

    Fri, Aug 23, 2024, 1:51 AM +01:00

---
+++ plates/plates.py
@@ -0,0 +1,36 @@
+def main():
+    plate = input("Enter Plate Number: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    # Check length of plate
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    # Check that the plate starts with at least two letters
+    if not s[0:2].isalpha():
+        return False
+
+    # Check for disallowed characters (periods, spaces, punctuation)
+    # if not s.isalnum():
+    #     return False
+
+    # Check the position of numbers
+    for i in range(len(s)):
+        if s[i].isdigit():
+            # Numbers must come at the end, no letters allowed after a number
+            if not s[i:].isdigit():
+                return False
+            # First number cannot be '0'
+            if s[i] == '0':
+                return False
+            break
+
+    return True
+
+
+main()
