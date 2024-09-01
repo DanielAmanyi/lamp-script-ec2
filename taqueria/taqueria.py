729d2c30ace1474b62ff456be30ee95514bda333
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-09-01 23:42:44 +0000

    Mon, Sep 2, 2024, 12:42 AM +01:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,30 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+new_menu = []
+bill = []
+def get_item():
+    for i in new_menu:
+        if i in menu:
+            bill.append(menu[i])
+    print(f"Total: ${sum(bill):.2f}")
+
+
+# list(menu)
+
+while True:
+    try:
+        item =input("Item:  ").title()
+        new_menu.append(item)
+    except EOFError:
+        get_item()
+        break
+
