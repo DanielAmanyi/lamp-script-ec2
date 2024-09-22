8d146ca1a50637d955fd99701d09d88ef9e70272
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-09-22 01:50:16 +0000

    Sun, Sep 22, 2024, 2:50 AM +01:00

---
+++ outdated/outdated.py
@@ -0,0 +1,39 @@
+import re
+calendar = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+month = ""
+date = ""
+year = ""
+# index = 0
+
+while len(date) != 2 and len(year) != 4:
+    try:
+        month, date, year = re.split(r"[,/]", input("Date: ").strip())
+        print("MM/DD/YYY")
+        month = month.title()
+
+    except ValueError:
+        # print("Invalid Entry")
+        continue
+
+
+for months in calendar:
+        if month == months:
+            month = calendar.index(month)+1
+        else:
+             month = month
+# print(index)
+print(f"{year}-{month:02}-{date}")
