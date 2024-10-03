d900204c47a24918e89e9f1f818f839901ca60b6
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-03 23:41:41 +0000

    Fri, Oct 4, 2024, 12:41 AM +01:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,7 +2,7 @@ import emoji
 
 english_text = input("Input: ")
 names = emoji.emojize(english_text)
-print(f"Output: {names}")
+print(f"Output: {names}",language='alias')
 
 
 # print(emoji.emojize('Python is :thumbs_up:'))
