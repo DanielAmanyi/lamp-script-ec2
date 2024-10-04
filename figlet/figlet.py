20cccc716fc368c86b4b877011923a668110a95c
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-04 00:59:32 +0000

    Fri, Oct 4, 2024, 1:59 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,13 +6,17 @@ g = Figlet()
 
 
 # Default Prompt
-text = input("Input: ")
-print("output:",g.renderText(text))
+for i in sys.argv:
+    if len(sys.argv) == 1:
+        text = input("Input: ")
+        print("output:",g.renderText(text))
+        break
 
 
 # Scan for Command Line Argument
 for i in sys.argv:
     if len(sys.argv) == 3:
         text = input("Input: ")
-    print ("output:",f.renderText(text))
+        print ("output:",f.renderText(text))
+        break
 
