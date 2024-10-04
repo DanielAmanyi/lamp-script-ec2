15c425c2b9f7adaf6d416a06d2df3fda73211c22
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-04 01:26:12 +0000

    Fri, Oct 4, 2024, 2:26 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -16,12 +16,12 @@ for i in sys.argv:
 # Scan for Command Line Argument
 for i in sys.argv:
     if len(sys.argv) == 3:
-    if sys.argv[1] == '-f' or '--f':
-        f = Figlet(font=sys.argv[1])
-        text = input("Input: ")
-        print ("output:",f.renderText(text))
-        sys.exit()
-    else:
-        print("Invalid usage")
-        sys.exit()
+        if sys.argv[1] == '-f' or '--f':
+            f = Figlet(font=sys.argv[1])
+            text = input("Input: ")
+            print ("output:",f.renderText(text))
+            sys.exit()
+        else:
+            print("Invalid usage")
+            sys.exit()
 
