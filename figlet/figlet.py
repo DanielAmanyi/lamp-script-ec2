3aa9832f2da03726ab52d3b2719916bf228de9fc
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-04 00:41:56 +0000

    Fri, Oct 4, 2024, 1:41 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,7 +5,7 @@ f = Figlet(font='slant')
 
 # elif sys.argv == 0:
 text = input("Input: ")
-print("output:",f.renderText(text))
+print("output:",Figlet(text))
 
 if sys.argv == 2:
     text = input("Input: ")
