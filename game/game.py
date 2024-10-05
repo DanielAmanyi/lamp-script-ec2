06772c4437b00fb54ce9cdf170f58ed0a8905237
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-05 01:48:06 +0000

    Sat, Oct 5, 2024, 2:48 AM +01:00

--- game/game.py
+++ game/game.py
@@ -3,11 +3,12 @@ level = 0
 guess = 0
 
 while level < 1 or guess < 1:
-    level = int(input("Level: "))
-    guess = int(input("Guess: "))
-    # if level and guess > 1:
-    #     break
-    # except ValueError:
-    #     continue
+    try:
+        level = int(input("Level: "))
+        guess = int(input("Guess: "))
+
+
+    except ValueError:
+        continue
 
 
