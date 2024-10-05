187d1ab7f23a2ab7922c8e9bcd560f59774d2be4
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-05 01:07:41 +0000

    Sat, Oct 5, 2024, 2:07 AM +01:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,6 +2,12 @@ import inflect
 p = inflect.engine()
 
 
+while True:
+    try:
+        name = input("Name: ")
+
+
+    except: 
 mylist = p.join(("apple", "banana"))
 mylist1 = p.join(("Liesl","Friedrich","Louisa"))
 # "apple, banana, and carrot"
