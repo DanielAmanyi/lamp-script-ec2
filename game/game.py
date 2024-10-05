f74598a416ba337082eefcc569f07f436c65bb8d
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-05 02:00:08 +0000

    Sat, Oct 5, 2024, 3:00 AM +01:00

--- game/game.py
+++ game/game.py
@@ -6,6 +6,13 @@ while level < 1 or guess < 1:
     try:
         level = int(input("Level: "))
         guess = int(input("Guess: "))
+        break
     except ValueError:
         continue
-if guess > random.randint(level)
+if guess > random.randint(level):
+    print("Too large!")
+elif guess < randomrandint(level):
+    print("Too low!")
+else:
+    print("Just righ!")
+
