9b4ccd383464dbb4a866690e4f4e5e2799b79156
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 20:29:12 +0000

    Wed, Oct 16, 2024, 9:29 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,33 +1,26 @@
-import sys
 def main():
-    # take in user input
+    # Take in user input
     while True:
-        try:
-            greeting = input("Hi Customer! \n").strip().lower()
-            if greeting:
-                print(value(greeting))
-                break
-            else:
-                continue
-        except ValueError or IndexError:
-            continue
-        sys.exit(0)
-
-
+        greeting = input("Hi Customer! \n").strip().lower()
+        # Ensure greeting is not empty
+        if greeting:
+            print(value(greeting))
+            break
+        else:
+            print("Please enter a valid greeting.")  # Prompt for valid input
 
 def value(greeting):
-    # logic to hunt for hello
+    # Logic to hunt for "hello"
     if "hello" in greeting:
         return "$0"
 
-    # logic to hunt for h
+    # Logic to hunt for words starting with "h"
     elif greeting[0] == "h":
         return "$20"
 
-    # counter reaction
+    # Counter reaction for all other cases
     else:
         return "$100"
-
 
 if __name__ == "__main__":
     main()
