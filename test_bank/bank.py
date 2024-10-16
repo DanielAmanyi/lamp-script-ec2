d1b553ff6b3be892b7d2d7f4b430a6509c18a8b6
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 16:00:09 +0000

    Wed, Oct 16, 2024, 5:00 PM +01:00

---
+++ test_bank/bank.py
@@ -0,0 +1,19 @@
+# def payout(h):
+    # twenty = "20"
+    # if h in reward[h]:
+    #     return twenty
+
+# take in user input
+greeting = input("Hi Customer! \n").strip().lower()
+
+#logic to hunt for hello
+if "hello" in greeting:
+    print("$0")
+
+# logic to hunt for h
+elif greeting[0] == "h":
+    print("$20")
+
+# counter reaction
+else:
+    print("$100")
