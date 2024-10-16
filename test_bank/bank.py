77d0127cde4e2e4ee330a1d7b8d98c29df045d2a
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 20:17:00 +0000

    Wed, Oct 16, 2024, 9:17 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -7,20 +7,21 @@ def main():
             break
         except ValueError:
             continue
+    return 0
 
 
 def value(greeting):
     # logic to hunt for hello
     if "hello" in greeting:
-        return("$0")
+        return "$0"
 
     # logic to hunt for h
     elif greeting[0] == "h":
-        return("$20")
+        return "$20"
 
     # counter reaction
     else:
-        return("$100")
+        return "$100"
 
 
 if __name__ == "__main__":
