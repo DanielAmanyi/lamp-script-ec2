621457fcabf008596a8c38be65e4d63d5a5edaec
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 19:52:59 +0000

    Wed, Oct 16, 2024, 8:52 PM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -3,14 +3,14 @@ from bank import value
 
 
 def test_value():
-    assert value("hello") == "$0"
+    assert value("hello").lower() == "$0"
 
 
 def test_notvalue():
     assert value("*") == "$100"
 
 def test_h():
-    assert value("h")     == "$20"
+    assert value("h").lower()     == "$20"
 
 def test_not_Hello():
-    assert value("Hello") or value("H") != "$100"
+    assert value("Hello") or value("H").lower() != "$100"
