f66193b200a70fb97546865ae2265c98cef0ec20
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 03:25:07 +0000

    Wed, Oct 16, 2024, 4:25 AM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,16 +1,24 @@
 # Vowels Library
-vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+def main():
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    # Take in user input
+    tweet = input("Enter your tweet: ")
+    # Convert tweet to a list
+    tweet = list(tweet)
+    shorten(tweet)
+    print(shor)
 
-# Take in user input
-tweet = input("Enter your tweet: ")
+def shorten(tweet):
 
-# Convert tweet to a list
-tweet = list(tweet)
-
-# Loop through each character in the tweet
-for i in tweet[:]:
+    # Loop through each character in the tweet
+    for i in tweet[:]:
     if i in vowels:
         tweet.remove(i)  # Remove the vowel
-
 # Convert the list back into a string and print the result
-print(''.join(tweet), end="")
+    new_tweet = (''.join(tweet), end="")
+    return new_tweet
+
+
+
+if __name__ == "__main__":
+    main()
