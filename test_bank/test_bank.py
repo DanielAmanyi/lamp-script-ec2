77fae82bae04301e6ca46f2d51af1d6191c8cd4f
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 19:30:21 +0000

    Wed, Oct 16, 2024, 8:30 PM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,2 +1,8 @@
 import pytest
-from bank import
+from bank import value
+
+
+def test_value():
+    assert value("Hello") == "$0"
+    assert value("H")     == "$20"
+    assert value("Hello") or value("H") != "$100"
