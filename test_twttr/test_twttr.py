8b1017c90489c32d4075e73e5f62681e354e1ff4
Daniel Amanyi <116466537+DanielAmanyi@users.noreply.github.com>
2024-10-16 13:24:25 +0000

    Wed, Oct 16, 2024, 2:24 PM +01:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,9 +5,16 @@ from twttr import shorten
 def test_shorten():
     assert shorten("hello") == "hll"
 
-
 def test_capson():
     assert shorten("HELLO WORLD") == "HLL WRLD"
 
 def test_camel_case():
     assert shorten("HEllo wOrlD") == "Hll WrlD"
+
+def main():
+    test_shorten()
+    test_capson()
+    test_camel_case()
+
+if __name__ == __main__:
+    main()
